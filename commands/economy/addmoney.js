56f64f774879ccc47f598dd1b3788dd8c7c8a6ad
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2021-10-24 09:09:30 -0400

    Changed embed color

--- commands/economy/addmoney.js
+++ commands/economy/addmoney.js
@@ -22,14 +22,14 @@ module.exports = {
 		database.addCoinsToMember(member, parseInt(args[1]), place);
 		if (args[1] < 0) {
 			let embed = new MessageEmbed()
-				.setColor('#ffe900')
+				.setColor('#0eea19')
 				.setTitle('✅ • Success')
 				.setDescription(`Removed \`$${Math.floor(args[1] * -1)}\` from **${member.user.tag}**'s ${place}.`)
 				.setAuthor(message.author.tag, message.author.avatarURL())
 			message.channel.send({ embeds: [embed] });
 		} else {
 			let embed = new MessageEmbed()
-				.setColor('#ffe900')
+				.setColor('#0eea19')
 				.setTitle('✅ • Success')
 				.setDescription(`Added \`$${Math.floor(args[1])}\` to **${member.user.tag}**'s ${place}.`)
 				.setAuthor(message.author.tag, message.author.avatarURL())
