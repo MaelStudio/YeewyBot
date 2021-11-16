2343e2e10bbc2b3626035f804c2b268c95d2850a
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2021-11-15 19:38:25 -0500

    Fixed unban command

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -4,14 +4,14 @@ module.exports = {
 	name: 'unban',
 	description: 'Unban a banned member from the server.',
 	usage: 'unban [member]',
-	args: { required: ['member'] },
+	args: { required: ['bannedMember'] },
 	permission: 'BAN_MEMBERS',
 	category: 'Moderation',
 	image: 'https://image.flaticon.com/icons/png/512/2061/premium/2061813.png',
 
 	async execute(message, args) {
         
-        const target = message.mentions.members.first() || message.guild.members.cache.find(m => m.user.username == args[0] || m.user.tag == args[0] || m.id == args[0]);
+        const target = message.guild.bans.cache.find(m => m.user.username == args[0] || m.user.tag == args[0] || m.id == args[0]);
         
 		if(!message.channel.permissionsFor(message.author, true).has('BAN_MEMBERS') && message.author.id !== message.guild.ownerId) {
 			let embed = new MessageEmbed()
