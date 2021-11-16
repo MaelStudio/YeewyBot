d4aaf1aa5152906e44b1e02d25d117c6f017749d
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2021-11-15 19:33:59 -0500

    Fixed ban command

--- commands/moderation/unban.js
+++ commands/moderation/unban.js
@@ -4,20 +4,30 @@ module.exports = {
 	name: 'unban',
 	description: 'Unban a banned member from the server.',
 	usage: 'unban [member]',
-	args: { required: ['bannedMember'] },
+	args: { required: ['member'] },
 	permission: 'BAN_MEMBERS',
 	category: 'Moderation',
 	image: 'https://image.flaticon.com/icons/png/512/2061/premium/2061813.png',
 
 	async execute(message, args) {
         
-        const target = message.guild.bans.cache.find(m => m.user.username == args[0] || m.user.tag == args[0] || m.id == args[0]);
+        const target = message.mentions.members.first() || message.guild.members.cache.find(m => m.user.username == args[0] || m.user.tag == args[0] || m.id == args[0]);
         
 		if(!message.channel.permissionsFor(message.author, true).has('BAN_MEMBERS') && message.author.id !== message.guild.ownerId) {
 			let embed = new MessageEmbed()
 				.setColor('#ff3a3a')
 				.setTitle('⚠️ • Error')
 				.setDescription('I do not have the permission to unban members.')
+				.setAuthor(message.author.tag, message.author.avatarURL())
+			message.channel.send({ embeds: [embed] });
+			return;
+		}
+
+        if(!message.guild.bans.get(target.id)) {
+			let embed = new MessageEmbed()
+				.setColor('#ff3a3a')
+				.setTitle('⚠️ • Error')
+				.setDescription('This member is not banned from the server.')
 				.setAuthor(message.author.tag, message.author.avatarURL())
 			message.channel.send({ embeds: [embed] });
 			return;
