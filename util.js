1e97beff022e499e0b52208fd1a5a35a256e8d30
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2021-12-18 17:43:02 -0500

    Add time converter function

--- util.js
+++ util.js
@@ -11,6 +11,19 @@ module.exports = {
 		return text2;
 	},
 
+	timeConverter(timestamp){
+		var a = new Date(timestamp);
+		var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+		var year = a.getUTCFullYear();
+		var month = months[a.getUTCMonth()];
+		var date = a.getUTCDate();
+		var hour = a.getUTCHours();
+		var min = a.getUTCMinutes();
+		var sec = a.getUTCSeconds();
+		var time = `${date} ${month} ${year} ${hour}:${min}:${sec} (UTC)`;
+		return time;
+	},
+
 	toMs: {
 		s: 1000,
 		m: 1000*60,
