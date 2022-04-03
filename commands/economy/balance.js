90262c4d2d23b80bfc640381508fada5f3c6c2c0
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2022-04-03 12:17:52 -0400

    Change user detection from arg

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const database = require('../../database.js');
-const util = requier('../../util.js')
+const util = require('../../util.js')
 
 module.exports = {
 	name: 'balance',
