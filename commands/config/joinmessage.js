e474d846a76ec5f18648fd1eb14dbd26e66adfc9
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2022-04-03 21:18:54 -0400

    Joinmessage command

--- commands/config/joinmessage.js
+++ commands/config/joinmessage.js
@@ -1,4 +1,5 @@
 const { MessageEmbed } = require('discord.js');
+const database = require('../../database.js');
 const util = require('../../util');
 
 module.exports = {
@@ -7,14 +8,22 @@ module.exports = {
 	usage: 'joinmessage [channel] [message]',
 	args: { required: ['channel', 'text'] },
 	permission: 'ADMINISTRATOR',
-	category: 'Config',
+	aliases: ['join-message'],
+	category: 'Configuration',
 	image: 'https://cdn-icons-png.flaticon.com/512/817/817615.png',
 
 	execute(message, args) {
 
 		const channel = util.getChannelFromArg(args[0], message.guild);
 		const joinMessage = args.slice(1).join(' ');
+		const dbGuild = await database.getGuild(message.guild);
 		
+		const join = {
+			message: joinMessage,
+			channel: channel.id
+		}
+		await dbGuild.updateOne({ join: join });
+
 		let embed = new MessageEmbed()
 			.setColor('#47ff4d')
 			.setTitle('✅ • Success')
