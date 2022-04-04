06a6b84fdf9d473584583d222f584f6ca51235e6
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2022-04-03 22:12:00 -0400

    Add disabledjoin command

---
+++ commands/config/disablejoin.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require('discord.js');
+const database = require('../../database.js');
+const util = require('../../util');
+
+module.exports = {
+	name: 'disablejoin',
+	description: 'Disables the server\'s join message.',
+	usage: 'disablejoin',
+	permission: 'ADMINISTRATOR',
+	aliases: ['disable-join'],
+	category: 'Configuration',
+	image: 'https://cdn-icons-png.flaticon.com/512/817/817615.png',
+
+	async execute(message, args) {
+
+		const dbGuild = await database.getGuild(message.guild);
+
+        if(!dbGuild.join) {
+            let embed = new MessageEmbed()
+				.setColor('#ff3a3a')
+				.setTitle('⚠️ • Error')
+				.setDescription('The server\'s join message is already disabled.')
+				.setAuthor(message.author.tag, message.author.avatarURL())
+			message.channel.send({ embeds: [embed] });
+            return;
+        }
+        
+		await dbGuild.updateOne({ join: {} });
+
+		let embed = new MessageEmbed()
+			.setColor('#47ff4d')
+			.setTitle('✅ • Success')
+			.setDescription(`The server\'s join message has been disabled.`)
+			.setAuthor(message.author.tag, message.author.avatarURL())
+		message.channel.send({ embeds: [embed] });
+
+	}
+}
