4776dd9e1bb5fd0cf11543e9d34b52a281c0cbe6
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2022-04-05 20:04:54 -0400

    Add guildMemberRemove event

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+const database = require('../database.js');
+
+module.exports = {
+    name: 'guildMemberRemove',
+    async execute (member) {
+        const dbGuild = await database.getGuild(member.guild);
+        if(!dbGuild.leave) return;
+
+        const channel = member.guild.channels.cache.get(dbGuild.leave.channel);
+        const message = dbGuild.leave.message;
+
+        if(dbGuild.leave.embed) {
+            let embed = new MessageEmbed()
+                .setColor('#47ff4d')
+                .setDescription(message)
+                .setAuthor(member.user.tag, member.user.avatarURL())
+            channel.send({ embeds: [embed] });
+        } else {
+            channel.send(message);
+        }
+        
+    }
+}
