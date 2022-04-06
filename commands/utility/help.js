1c037276f4aa1345f4c9f0ce4e01ad0bcd047d64
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2022-04-05 21:23:55 -0400

    Change image

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -31,7 +31,7 @@ module.exports = {
 				.setColor('#ffb5ed')
 				.setTitle('Help Page')
 				.setAuthor(message.author.tag, message.author.avatarURL())
-				.setThumbnail('https://media.discordapp.net/attachments/772805402399604756/823642462110220308/yeewy-rainbow.png')
+				.setThumbnail('https://i.imgur.com/aoA4Bgi.png')
 				.setFooter(`Use y.help <command> to get info on a specific command`)
 			
 			Object.keys(commandsList).forEach(function(key) {
