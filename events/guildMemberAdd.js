ff67cc6962452c1303c2e51c4a54e02ff339093a
MaelStudio <66912947+MaelStudio@users.noreply.github.com>
2022-07-08 00:15:49 -0400

    Assign autorole

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,33 +4,42 @@ const database = require('../database.js');
 module.exports = {
     name: 'guildMemberAdd',
     async execute (member) {
+
         const dbGuild = await database.getGuild(member.guild);
-        if(!dbGuild.join) return;
 
-        const channel = member.guild.channels.cache.get(dbGuild.join.channel);
-        let message = dbGuild.join.message;
+        // join message
+        if(dbGuild.join) {
+            const channel = member.guild.channels.cache.get(dbGuild.join.channel);
+            let message = dbGuild.join.message;
 
-        // replace variables
-        variables = {
-            '[member.username]': member.user.username,
-            '[member.tag]': member.user.tag,
-            '[member.mention]': `<@${member.id}>`,
-            '[server.name]': member.guild.name,
-            '[server.memberCount]': member.guild.memberCount.toString()
+            // replace variables
+            variables = {
+                '[member.username]': member.user.username,
+                '[member.tag]': member.user.tag,
+                '[member.mention]': `<@${member.id}>`,
+                '[server.name]': member.guild.name,
+                '[server.memberCount]': member.guild.memberCount.toString()
+            }
+
+            for (const v in variables) {
+                message = message.replace(v, variables[v])
+            }
+
+            if(dbGuild.join.embed) {
+                let embed = new MessageEmbed()
+                    .setColor('#47ff4d')
+                    .setDescription(message)
+                    .setAuthor(member.user.tag, member.user.avatarURL())
+                channel.send({ embeds: [embed] });
+            } else {
+                channel.send(message);
+            }
         }
 
-        for (const v in variables) {
-            message = message.replace(v, variables[v])
-        }
-
-        if(dbGuild.join.embed) {
-            let embed = new MessageEmbed()
-                .setColor('#47ff4d')
-                .setDescription(message)
-                .setAuthor(member.user.tag, member.user.avatarURL())
-            channel.send({ embeds: [embed] });
-        } else {
-            channel.send(message);
+        // assign autorole
+        if(dbGuild.autorole) {
+            const role = member.guild.roles.get(dbguild.autorole);
+            member.roles.add(role);
         }
         
     }
